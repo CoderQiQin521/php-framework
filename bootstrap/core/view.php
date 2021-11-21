bcf49754854489e1b077199ab50d0ac3597b5c0c
CoderQiQin521 <coderqiqin@aliyun.com>
2021-11-21 21:49:14 +0800

    模版引擎

--- bootstrap/core/view.php
+++ bootstrap/core/view.php
@@ -14,8 +14,12 @@ class view
     {
         $file = BASE . '/views/' . $template;
         if (is_file($file)) {
-            extract($data);
-            include $file;
+//            extract($data);
+//            include $file;
+            $loader = new \Twig\Loader\FilesystemLoader(BASE . '/views');
+            $twig = new \Twig\Environment($loader);
+    
+            echo $twig->render('index.html', $data);
         }else {
             throw new \Exception('模版不存在'.$template);
         }
