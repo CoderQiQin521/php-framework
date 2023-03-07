29442553cda30c65e7f202a55956b5d96958778c
CoderQiQin <coderqiqin@aliyun.com>
2023-03-07 09:11:21 +0800

    add unit test

--- tests/TestClass.php
+++ tests/TestClass.php
@@ -1,6 +1,12 @@
 <?php
 
+use TestCase;
 // unit test
 class TestClass extends TestCase {
 
+    public function TestAdd() {
+        $a = 10;
+        $B = 20;
+        $this->assertEquals(30, $a + $B);
+    }
 }
