0c7d1c590ecf87cd94938df56bc5af76ff3e65dc
zhangbuda <59273443+18211821001@users.noreply.github.com>
2023-10-10 09:06:38 +0800

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -19,3 +19,45 @@ int main (){
 
 
 echo 123;
+https://zhuanlan.zhihu.com/p/375592165
+
+#include <stdio.h>
+ 
+int main (){
+  /* 局部变量声明 */
+  int a, b;
+  int c;
+ 
+  /* 实际初始化 */
+  a = 10;
+  b = 20;
+  c = a + b;
+ 
+  printf ("value of a = %d, b = %d and c = %d\n", a, b, c);
+ 
+  return 0;
+}
+
+
+echo 123;
+https://zhuanlan.zhihu.com/p/375592165
+
+#include <stdio.h>
+ 
+int main (){
+  /* 局部变量声明 */
+  int a, b;
+  int c;
+ 
+  /* 实际初始化 */
+  a = 10;
+  b = 20;
+  c = a + b;
+ 
+  printf ("value of a = %d, b = %d and c = %d\n", a, b, c);
+ 
+  return 0;
+}
+
+
+echo 123;
