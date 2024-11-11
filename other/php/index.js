7016e8968e199fb05b8ab5b3708dc3b959ac69f4
Qi Qin <coderqiqin@aliyun.com>
2024-11-11 09:05:29 +0800

    Update index.js

--- other/php/index.js
+++ other/php/index.js
@@ -16,6 +16,11 @@ console.log(arr[1])
 
 function hello() {
 }
+                                                                                const arr = [1,2,3,4];
+console.log(arr[1])
+
+function hello() {
+}
 console.log(1234567)
 
 console.log(1234567)
